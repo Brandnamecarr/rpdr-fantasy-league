63dadbe4f3ec9e753be7523c2a786a39c0669f2c
Brandon <Brandnamecarr@ufl.edu>
2025-12-26 14:13:10 -0800

    Align types in appTypes.py

--- utilities/Python Tools/appTypes.py
+++ utilities/Python Tools/appTypes.py
@@ -16,14 +16,19 @@ class League:
     id = 0
     leagueName = ''
     leagueOwner = ''
+    franchise = ''
+    season = 0
     users = [User]
     maxPlayers = 0
     createdOn = None
+    maxQueensPerTeam = 0
 
-    def __init__(self, id, leagueName, leagueOwner, users, maxPlayers, createdOn):
+    def __init__(self, id, leagueName, leagueOwner, franchise, season, users, maxPlayers, createdOn):
         self.id = id
         self.leagueName = leagueName
         self.leaugeOwner = leagueOwner
+        self.franchise = franchise
+        self.season = season
         self.users = users
         self.maxPlayers = maxPlayers
         self.createdOn = createdOn
@@ -33,16 +38,22 @@ class Roster:
     leagueName = ''
     username = ''
     teamName = ''
+    franchise = ''
+    season = 0
     queens = []
     currentPoints = 0
+    pointUpdates = []
 
-    def __init__(self, id, leagueName, username, teamName, queens, currentPoints):
+    def __init__(self, id, leagueName, username, teamName, franchise, season, queens, currentPoints, pointUpdates):
         self.id = id
         self.leagueName = leagueName
         self.username = username
         self.teamName = teamName
+        self.franchise = franchise
+        self.season = season
         self.queens = queens
         self.currentPoints = currentPoints
+        self.pointUpdates = pointUpdates
 
 class AuthTokens:
     
@@ -54,4 +65,38 @@ class AuthTokens:
         self.userID = id
         self.email = email
         self.token = token
+
+class Notification:
+    notifId = 0
+    source = ''
+    destination = ''
+    resolved = False
+    content = ''
+    createdAt = None
+    updatedAt = None
+
+    def __init__(self, notifId, source, destination, resolved, content, createdAt, updatedAt):
+        self.notifId = notifId
+        self.source = source
+        self.destination = destination
+        self.resolved = resolved
+        self.content = content
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+
+class Queen:
+    queenId = 0
+    franchise = ''
+    season = 0
+    name = ''
+    status = "ACTIVE" # QueenStatus enum #
+    location = ''
+
+    def __init__(self, queenId, franchise, season, name, status, location):
+        self.queenId = queenId
+        self.franchise = franchise
+        self.season = season
+        self.name = name
+        self.status = status
+        self.location = location
     
